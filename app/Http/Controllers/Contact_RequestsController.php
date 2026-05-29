fa7489ae53bb0b3dd348fcc370357f4e641f2f3e
CodeBySourav <souravprasad.co.in@gmail.com>
2026-05-29 16:36:09 +0530

    mail from

--- app/Http/Controllers/Contact_RequestsController.php
+++ app/Http/Controllers/Contact_RequestsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ContactRequest;
+use App\Mail\ContactRequestMail;
+use Illuminate\Support\Facades\Mail; 
 
 class Contact_RequestsController extends Controller
 {
@@ -22,6 +24,15 @@ class Contact_RequestsController extends Controller
             'phone' => $request->phone,
             'info'  => $request->info,
         ]);
+
+        $data = [
+            'name'  => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'info'  => $request->info,
+        ];
+
+        Mail::to('yourmail@gmail.com')->send(new ContactRequestMail($data));
 
         return back()->with('success', 'Thanks for reaching out! Our team will contact you shortly.');
     }
---
+++ resources/views/emails/contact_request.blade.php
@@ -0,0 +1,6 @@
+<h2>New Contact Request</h2>
+
+<p><strong>Name:</strong> {{ $data['name'] }}</p>
+<p><strong>Email:</strong> {{ $data['email'] }}</p>
+<p><strong>Phone:</strong> {{ $data['phone'] }}</p>
+<p><strong>Service:</strong> {{ $data['info'] }}</p>
